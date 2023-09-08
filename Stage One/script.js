418c15744f7735b24112019e6af7ef2226dc63c8
Stealthboyy <olajideokepelumi2002@gmail.com>
2023-09-08 10:57:59 +0100

    commit

--- Stage One/script.js
+++ Stage One/script.js
@@ -2,6 +2,10 @@ const utcTime = document.querySelector('[data-testid="currentUTCTime"]');
 
 // days of the week with index (0 for Sunday, 1 for Monday, and so on).
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const myTrackUpdated = document.querySelector('[data-testid="myTrack"]');
+const myUsernameUpdated = document.querySelector('[data-testid="slackUserName"]');
+const myPortfolio = document.querySelector('[data-testid=“githubURL”]');
+const myProfilePicture = document.querySelector('[data-testid="slackDisplayImage"]');
 
 updateDayOfTheWeek = () => {
     const currentDate = new Date(); 
@@ -19,11 +23,29 @@ function updateUTCTime() {
 }
 
 // Update the UTC time and day of the week every second
+
+
+updatePersonalData = () => {
+myTrackUpdated.textContent = "Frontend"; 
+myUsernameUpdated.textContent = "Olajide-Oke Oluwapelumi"
+
+// Github Link
+myPortfolio.setAttribute('href', 'https://github.com/stealthboyy/zuri-intern');
+myPortfolio.textContent = "My Github Profile"
+
+// Display Picture
+myProfilePicture.setAttribute('src', './assets/pictures/slackDisplayPicture.png');
+myProfilePicture.setAttribute('alt', 'Olajide-Oke Oluwapelumi');
+}
+
+
+
+
+
+
+updatePersonalData();
+
 setInterval(() => {
     updateUTCTime();
     updateDayOfTheWeek();
 }, 1000);
-
-// Example: Update the track
-const trackElement = document.querySelector('[data-testid="myTrack"]');
-trackElement.textContent = "Frontend"; 
